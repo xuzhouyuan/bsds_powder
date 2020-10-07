3aa64cceb6d8da0c3c8c7a7007e60ae282164ecd
Zhouyuan Xu <xuzhouyuan@XuMacBook-Pro.local>
2020-10-07 15:11:52 -0700

    other: added submissions for assignment 1

---
+++ submissions/assignment-1/client-part-2/powder_client/src/main/java/LoadGenerator.java
@@ -0,0 +1,236 @@
+import io.swagger.client.ApiClient;
+import io.swagger.client.ApiException;
+import io.swagger.client.ApiResponse;
+import io.swagger.client.api.SkiersApi;
+import io.swagger.client.model.LiftRide;
+import io.swagger.client.model.SkierVertical;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CountDownLatch;
+
+public class LoadGenerator implements Runnable{
+    private String serverPath;
+    private List<CountDownLatch> latches;
+    private String resortID;
+    private String dayId;
+    private int numThreads;
+    private int numLifts;
+    private int startSkierId, endSkierId;
+    private int startMinute, endMinute;
+    private boolean coolDown = false;
+
+    private List<RequestCount> requestCounter;
+    private RequestCount requestCount;
+    private BlockingQueue<String> latencyQueue;
+    private static Logger logger = LogManager.getLogger(PowderClient.class);
+
+    public LoadGenerator(String serverPath, List<CountDownLatch> latches, RequestCount requestCount,
+                         BlockingQueue<String> latencyQueue, int numThreads,String resortID, int dayId, int numLifts,
+                         int startSkierId, int endSkierId, int startMinute, int endMinute) {
+        this.serverPath = serverPath;
+        this.latches = latches;
+        this.requestCount = requestCount;
+        this.latencyQueue = latencyQueue;
+        this.numThreads = numThreads;
+        this.resortID = resortID;
+        this.dayId = Integer.toString(dayId);
+        this.numLifts = numLifts;
+        this.startSkierId = startSkierId;
+        this.endSkierId = endSkierId;
+        this.startMinute = startMinute;
+        this.endMinute = endMinute;
+        requestCounter = new ArrayList<>(numThreads);
+    }
+
+    public LoadGenerator(String serverPath, List<CountDownLatch> latches, RequestCount requestCount,
+                         BlockingQueue<String> latencyQueue, int numThreads, String resortID, int dayId, int numLifts,
+                         int startSkierId, int endSkierId, int startMinute, int endMinute,
+                         boolean coolDown) {
+        this(serverPath, latches, requestCount, latencyQueue, numThreads, resortID, dayId, numLifts,
+                startSkierId, endSkierId, startMinute, endMinute);
+        this.coolDown = coolDown;
+    }
+
+    @Override
+    public void run() {
+        int numSkiers = endSkierId - startSkierId;
+        if (coolDown) {
+            logger.info("Starting generate loads with LoadWorkerCoolDown");
+            for (int i = 0; i < numThreads; i++) {
+                RequestCount requestCount = new RequestCount();
+                requestCounter.add(requestCount);
+                // calculate skierId range for different workers
+                int workerStartSkierId = startSkierId + i * (numSkiers / numThreads);
+                int workerEndSkierId = startSkierId + (i + 1) * (numSkiers / numThreads);
+                LoadWorkerCoolDown worker = new LoadWorkerCoolDown(latches, serverPath, requestCount,
+                        latencyQueue, resortID, dayId, numLifts, workerStartSkierId, workerEndSkierId,
+                        startMinute, endMinute);
+                new Thread(worker).start();
+            }
+        } else {
+            logger.info("Starting generate loads with LoadWorker");
+            for (int i = 0; i < numThreads; i++) {
+                RequestCount requestCount = new RequestCount();
+                requestCounter.add(requestCount);
+                // calculate skierId range for different workers
+                int workerStartSkierId = startSkierId + i * (numSkiers / numThreads);
+                int workerEndSkierId = startSkierId + (i + 1) * (numSkiers / numThreads);
+                LoadWorker worker = new LoadWorker(latches, serverPath, requestCount,
+                        latencyQueue, resortID, dayId, numLifts, workerStartSkierId, workerEndSkierId,
+                        startMinute, endMinute);
+                new Thread(worker).start();
+            }
+        }
+    }
+
+    public void sumRequestCounter() {
+        for (int i = 0; i < requestCounter.size(); i++) {
+            RequestCount workerCount = requestCounter.get(i);
+            requestCount.success += workerCount.success;
+            requestCount.failure += workerCount.failure;
+        }
+    }
+}
+
+class LoadWorker implements Runnable {
+    protected List<CountDownLatch> latches;
+    protected String serverPath;
+    protected RequestCount requestCount;
+    protected BlockingQueue<String> latencyQueue;
+    protected Random rand;
+    protected static Logger logger = LogManager.getLogger(PowderClient.class);
+
+    protected String resortId;
+    protected String dayId;
+    protected int numLifts;
+    protected int startSkierId, endSkierId;
+    protected int startMinute, endMinute;
+
+    protected int numPOST = 100;
+    protected int numGET = 5;
+
+    public LoadWorker(List<CountDownLatch> latches, String serverPath, RequestCount requestCount,
+                      BlockingQueue<String> latencyQueue, String resortId, String dayId, int numLifts,
+                      int startSkierId, int endSkierId, int startMinute, int endMinute) {
+        this.latches = latches;
+        this.serverPath = serverPath;
+        this.requestCount = requestCount;
+        this.latencyQueue = latencyQueue;
+        this.resortId = resortId;
+        this.dayId = dayId;
+        this.numLifts = numLifts;
+        this.startSkierId = startSkierId;
+        this.endSkierId = endSkierId;
+        this.startMinute = startMinute;
+        this.endMinute = endMinute;
+        rand = new Random();
+    }
+
+    @Override
+    public void run() {
+        ApiClient apiClient = new ApiClient();
+        apiClient.setBasePath(serverPath);
+        SkiersApi api = new SkiersApi(apiClient);
+        logger.info(this.getClass().getName() + "starting to create loads.");
+        generatePOSTs(api);
+        generateGETs(api);
+        for (int i = 0; i < latches.size(); i++) {
+            latches.get(i).countDown();
+        }
+    }
+
+    protected void generatePOSTs(SkiersApi api) {
+        int successCount = 0;
+        int failureCount = 0;
+        for (int i = 0; i < numPOST; i++) {
+            try {
+                int skierId = startSkierId + rand.nextInt(endSkierId-startSkierId);
+                int liftId = rand.nextInt(numLifts) + 1;
+                int minute = startMinute + rand.nextInt(endMinute-startMinute);
+                LiftRide ride = packLiftRide(liftId, skierId, minute);
+                long startNano = System.nanoTime();
+                ApiResponse<Void> response = api.writeNewLiftRideWithHttpInfo(ride);
+                long endNano = System.nanoTime();
+                int latency = (int) ((endNano - startNano) / 1000000); // millisecond
+                String responseCode = Integer.toString(response.getStatusCode());
+                String latencyEntry = startNano + ",POST," + latency + "," + responseCode;
+                if (!latencyQueue.offer(latencyEntry)) {
+                    logger.error("latencyQueue is full, latency entry not logged: " + latencyEntry);
+                }
+                if (responseCode.startsWith("4") || responseCode.startsWith("5")) {
+                    logger.error(this.getClass().getName() + " received status code " + responseCode +
+                            " when calling POST writeNewLiftRide");
+                    failureCount++;
+                } else {
+                    successCount++;
+                }
+            } catch (ApiException e) {
+                System.err.println("Exception when calling writeNewLiftRide");
+                failureCount++;
+                e.printStackTrace();
+            }
+        }
+        requestCount.success += successCount;
+        requestCount.failure += failureCount;
+    }
+
+    protected void generateGETs(SkiersApi api) {
+        int successCount = 0;
+        int failureCount = 0;
+        for (int i = 0; i < numGET; i++) {
+            try {
+                int skierId = startSkierId + rand.nextInt(endSkierId-startSkierId);
+                long startNano = System.nanoTime();
+                ApiResponse<SkierVertical> response = api.getSkierDayVerticalWithHttpInfo(resortId, dayId,
+                        Integer.toString(skierId));
+                long endNano = System.nanoTime();
+                int latency = (int) ((endNano - startNano) / 1000000); // millisecond
+                String responseCode = Integer.toString(response.getStatusCode());
+                String latencyEntry = startNano + ",GET," + latency + "," + responseCode;
+                if (!latencyQueue.offer(latencyEntry)) {
+                    logger.error("latencyQueue is full, latency entry not logged: " + latencyEntry);
+                }
+                if (responseCode.startsWith("4") || responseCode.startsWith("5")) {
+                    logger.error(this.getClass().getName() + " received status code " + responseCode +
+                            " when calling GET getSkierDayVertical");
+                    failureCount++;
+                } else {
+                    successCount++;
+                }
+            } catch (ApiException e) {
+                System.err.println("Exception when calling getSkierDayVertical");
+                failureCount++;
+                e.printStackTrace();
+            }
+        }
+        requestCount.success += successCount;
+        requestCount.failure += failureCount;
+    }
+
+    protected LiftRide packLiftRide(int liftId, int skierId, int minute) {
+        LiftRide ride = new LiftRide();
+        ride.setDayID(dayId);
+        ride.setLiftID(Integer.toString(liftId));
+        ride.setResortID(resortId);
+        ride.setSkierID(Integer.toString(skierId));
+        ride.setTime(Integer.toString(minute));
+        return ride;
+    }
+}
+
+class LoadWorkerCoolDown extends LoadWorker {
+
+    public LoadWorkerCoolDown(List<CountDownLatch> latches, String serverPath, RequestCount requestCount,
+                              BlockingQueue<String> latencyQueue, String resortId, String dayId, int numLifts,
+                              int startSkierId, int endSkierId, int startMinute, int endMinute) {
+        super(latches, serverPath, requestCount, latencyQueue, resortId, dayId, numLifts,
+                startSkierId, endSkierId, startMinute, endMinute);
+        numGET = 10;
+    }
+}
+
---
+++ submissions/assignment-1/client-part-2/powder_client/src/main/java/PowderClient.java
@@ -0,0 +1,226 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class PowderClient {
+    private final String SCHEME = "http://";
+    private String serverPath = SCHEME + "18.237.250.178:8080/powder/";
+    private int maxThreads = 256;
+    private int numSkiers = 20000;
+    private int numLifts = 40;
+    private int skiDay = 1;
+    private String resortID = "SilverMt";
+
+    private static Logger logger = LogManager.getLogger(PowderClient.class);
+
+    public PowderClient() {
+        setParameters();
+    }
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        // initialize client
+        PowderClient client = new PowderClient();
+        BlockingQueue<String> latencyQueue = new LinkedBlockingQueue<>();
+        client.runPhases(latencyQueue);
+    }
+
+    /**
+     * @return The total number of requests made
+     */
+    public int runPhases(BlockingQueue<String> latencyQueue) throws InterruptedException, IOException {
+        int numThreadsPhase1 = maxThreads / 4;
+        int numThreadsPhase2 = maxThreads;
+        int numThreadsPhase3 = maxThreads / 4;
+        CountDownLatch phase1End = new CountDownLatch(numThreadsPhase1);
+        CountDownLatch phase2Start = new CountDownLatch((numThreadsPhase1 + 10 - 1) / 10);
+        CountDownLatch phase2End = new CountDownLatch(numThreadsPhase2);
+        CountDownLatch phase3Start = new CountDownLatch((numThreadsPhase2 + 10 - 1) / 10);
+        CountDownLatch phase3End = new CountDownLatch(numThreadsPhase3);
+        List<CountDownLatch> phase1Latches = Arrays.asList(phase1End, phase2Start);
+        List<CountDownLatch> phase2Latches = Arrays.asList(phase2End, phase3Start);
+        List<CountDownLatch> phase3Latches = Arrays.asList(phase3End);
+        RequestCount requestCountPhase1 = new RequestCount();
+        RequestCount requestCountPhase2 = new RequestCount();
+        RequestCount requestCountPhase3 = new RequestCount();
+
+        LoadGenerator phase1 = new LoadGenerator(serverPath, phase1Latches, requestCountPhase1,
+                latencyQueue, numThreadsPhase1, resortID, skiDay, numLifts,
+                0, numSkiers, 1, 91);
+        LoadGenerator phase2 = new LoadGenerator(serverPath, phase2Latches, requestCountPhase2,
+                latencyQueue, numThreadsPhase2, resortID, skiDay, numLifts,
+                0, numSkiers, 91, 361);
+        LoadGenerator phase3 = new LoadGenerator(serverPath, phase3Latches, requestCountPhase3,
+                latencyQueue, numThreadsPhase3, resortID, skiDay, numLifts,
+                0, numSkiers, 361, 421, true);
+
+        logger.info(this.getClass().getName() + " starts to run phases.");
+        logger.info(this.getClass().getName() + " serverPath is " + serverPath);
+        System.out.println("phase 1 starting");
+        long startNano = System.nanoTime();
+        new Thread(phase1).start();
+        phase2Start.await();
+        System.out.println("phase 2 starting");
+        new Thread(phase2).start();
+        phase3Start.await();
+        System.out.println("phase 3 starting");
+        new Thread(phase3).start();
+        phase1End.await();
+        phase2End.await();
+        phase3End.await();
+        long endNano = System.nanoTime();
+        phase1.sumRequestCounter();
+        phase2.sumRequestCounter();
+        phase3.sumRequestCounter();
+
+        System.out.println("All phases completed");
+        int success = requestCountPhase1.success + requestCountPhase2.success + requestCountPhase3.success;
+        int failure = requestCountPhase1.failure + requestCountPhase2.failure + requestCountPhase3.failure;
+        int totalRequest = success + failure;
+        System.out.println("Total number successful requests: " + success);
+        System.out.println("Total number of failed requests: " + failure);
+        int wallTime = (int) ((endNano - startNano) / 1000000); // millisecond
+        System.out.println("Total run time (wall time): " + wallTime + " ms");
+        float throughput = ((float)totalRequest / wallTime) * 1000;
+        System.out.println("Throughput is: " + throughput + " req/sec");
+        processLatencyStat(latencyQueue);
+        return totalRequest;
+    }
+
+    private String[] processLatencyStat(BlockingQueue<String> latencyQueue) throws IOException {
+        // TODO: [in the future] create thread to write latencyQueue to csv file,
+        //  process stats from file after completion.
+        List<Integer> latencyPOST = new ArrayList<>();
+        List<Integer> latencyGET = new ArrayList<>();
+        File csvOutputPOST = File.createTempFile("powder-client-request-latency-POST-", ".csv");
+        File csvOutputGET = File.createTempFile("powder-client-request-latency-GET-", ".csv");
+        System.out.println("POST latency entries stored at " + csvOutputPOST.getAbsolutePath());
+        System.out.println("GET latency entries stored at " + csvOutputGET.getAbsolutePath());
+        FileWriter writerPOST = new FileWriter(csvOutputPOST.getAbsolutePath());
+        FileWriter writerGET = new FileWriter(csvOutputGET.getAbsolutePath());
+        while (!latencyQueue.isEmpty()) {
+            String entryString = latencyQueue.poll();
+            String[] entry = entryString.split(",");
+            if (entry[1].equals("POST")) {
+                writerPOST.write(entryString+"\n");
+                latencyPOST.add(Integer.parseInt(entry[2]));
+            } else if (entry[1].equals("GET")) {
+                writerGET.write(entryString+"\n");
+                latencyGET.add(Integer.parseInt(entry[2]));
+            }
+        }
+        writerPOST.close();
+        writerGET.close();
+        Collections.sort(latencyPOST);
+        Collections.sort(latencyGET);
+
+        System.out.println("Latency (ms) statistics for POST: ");
+        System.out.print("  MEAN  : ");
+        System.out.println(findMean(latencyPOST));
+        System.out.print("  MEDIAN: ");
+        System.out.println(findMedian(latencyPOST));
+        System.out.print("  P99   : ");
+        System.out.println(findP99(latencyPOST));
+        System.out.print("  MAX   : ");
+        System.out.println(latencyPOST.get(latencyPOST.size()-1));
+
+        System.out.println("Latency (ms) statistics for GET: ");
+        System.out.print("  MEAN  : ");
+        System.out.println(findMean(latencyGET));
+        System.out.print("  MEDIAN: ");
+        System.out.println(findMedian(latencyGET));
+        System.out.print("  P99   : ");
+        System.out.println(findP99(latencyGET));
+        System.out.print("  MAX   : ");
+        System.out.println(latencyGET.get(latencyGET.size()-1));
+        return new String[] {csvOutputPOST.getAbsolutePath(), csvOutputGET.getAbsolutePath()};
+    }
+
+    private int findMedian(List<Integer> list) {
+        int length = list.size();
+        if (length % 2 == 0) {
+            return (int) Math.round((list.get(length/2) + list.get((length/2)+1)) / 2.0);
+        }
+        return list.get(length/2);
+    }
+
+    private int findMean(List<Integer> list) {
+        return (int) Math.round(list.stream().mapToInt(i -> i).average().orElse(-1.0));
+    }
+
+    private Integer findP99(List<Integer> list) {
+        if (list.size() == 0) {
+            return -1;
+        }
+        return list.get(list.size() * 99 / 100);
+    }
+
+    // set parameters of the client from console inputs
+    private void setParameters() {
+        Scanner inputScan = new Scanner(System.in);
+
+        System.out.println("Please enter the maximum number of threads to run (Default 256): ");
+        try {
+            int inputMaxThreads = Integer.parseInt(inputScan.nextLine());
+            if (inputMaxThreads <= 0) throw new NumberFormatException();
+            this.maxThreads = inputMaxThreads;
+        } catch (NumberFormatException e) {
+            System.out.println("default value used");
+        }
+        System.out.println("maxThreads is " + this.maxThreads + "\n");
+
+        System.out.println("Please enter the number of skiers (Default 50,000):");
+        try {
+            int inputNumSkiers = Integer.parseInt(inputScan.nextLine());
+            if (inputNumSkiers <= 0) throw new NumberFormatException();
+            this.numSkiers = inputNumSkiers;
+        } catch (NumberFormatException e) {
+            System.out.println("default value used");
+        }
+        System.out.println("numSkiers is " + this.numSkiers + "\n");
+
+        System.out.println("Please enter the number of ski lifts (Range 5 - 60, default 40):");
+        try {
+            int inputNumLifts = Integer.parseInt(inputScan.nextLine());
+            if (inputNumLifts < 5 || inputNumLifts > 60) throw new NumberFormatException();
+            this.numLifts = inputNumLifts;
+        } catch (NumberFormatException e) {
+            System.out.println("default value used");
+        }
+        System.out.println("numLifts is " + this.numLifts + "\n");
+
+        System.out.println("Please enter the ski day (Default 1):");
+        try {
+            int inputSkiDay = Integer.parseInt(inputScan.nextLine());
+            if (inputSkiDay <= 0) throw new NumberFormatException();
+            this.skiDay = inputSkiDay;
+        } catch (NumberFormatException e) {
+            System.out.println("default value used");
+        }
+        System.out.println("skiDay is " + this.skiDay + "\n");
+
+        System.out.println("Please enter the the resort name (Default SilverMt):");
+        String inputResortId = inputScan.nextLine();
+        if (inputResortId.isEmpty()) {
+            System.out.println("default value used");
+        } else {
+            this.resortID = inputResortId;
+        }
+        System.out.println("resortId is " + this.resortID + "\n");
+
+        System.out.println("Please enter address of the server including port (Default localhost):");
+        String inputServerPath = inputScan.nextLine();
+        if (inputServerPath.isEmpty()) {
+            System.out.println("default value used");
+        } else {
+            this.serverPath = this.SCHEME + inputServerPath;
+        }
+        System.out.println("serverPath is " + this.serverPath + "\n");
+    }
+}
